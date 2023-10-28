55078219d484a6e74ffb69fc480df05676b253c7
MOAIZ-UL-ISLAM <moaiz0404@gmail.com>
2023-10-28 13:38:39 +0500

    admin configuration

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Catagory,Post
 # Register your models here.
-admin.site.register(Catagory)
-admin.site.register(Post)
+
+#custom Categoryadmin Dashboard
+class CatagoryAdmin(admin.ModelAdmin):
+    list_display=('image_tag','title','description','add_date')
+    search_fields=('title',)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','cat','post_id')
+
+
+# registered Models
+admin.site.register(Catagory, CatagoryAdmin)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,24 @@
 from django.db import models
-
+from django.utils.html import format_html
 # Create your models here.
+
+
 class Catagory(models.Model):
     cat_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    url =models.CharField(blank=True,max_length=100)
+    url = models.CharField(blank=True, max_length=100)
     image = models.ImageField(upload_to='catalog/')
-    add_date = models.DateTimeField(auto_now_add=True,null=True)
+    add_date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def image_tag(self):
+        return format_html('<img src="/media/{}"  alt="image" styles="width:20px;heigth:20px;border-radius:50%" />'.format(self.image))
 
 
 class Post(models.Model):
-    post_id= models.AutoField(primary_key=True)
+    post_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    url =models.CharField(max_length=100)
-    cat = models.ForeignKey(Catagory,on_delete=models.CASCADE)
+    url = models.CharField(max_length=100)
+    cat = models.ForeignKey(Catagory, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post/')
